5864e7597bf33c67d73c07c3504ff98846f48752
Puneeth Gowda DS <puneeth5577@gmail.com>
2024-08-08 21:20:02 +0530

    third commit-update version

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,14 +2,15 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAj45kqSq0Jja2ZXDkwmj-qpN6aD9596_4",
-  authDomain: "smart-room-booking-5149e.firebaseapp.com",
-  projectId: "smart-room-booking-5149e",
-  storageBucket: "smart-room-booking-5149e.appspot.com",
-  messagingSenderId: "186744359197",
-  appId: "1:186744359197:web:526c1598b0fee6f53279f4",
-  measurementId: "G-BT4SLTP9KY"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
