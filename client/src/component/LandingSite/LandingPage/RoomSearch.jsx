d50be5a74d9822dc83a3d5e6bf9292696c7c7295
Puneeth Gowda DS <puneeth5577@gmail.com>
2024-09-29 23:41:10 +0530

    UI/UX update

--- client/src/component/LandingSite/LandingPage/RoomSearch.jsx
+++ client/src/component/LandingSite/LandingPage/RoomSearch.jsx
@@ -1,25 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "../../../component/Component.css"
+import "../LandingPage/RoomSearch.css";
+
 const RoomSearch = () => {
-
-
   return (
     <div>
       <div className="homepage">
-      <div className="hero-image px-5 py-2 ml-5">
-        <img src="../../images/jpeg/defaultBcg2.jpg" alt="Beach Resort" style={{width:1600, height : 800}}/>
-      </div>
+        <div className="hero-image">
+          <div className="content">
+          <h1>Luxurious Rooms</h1>
+          <p>Deluxe Rooms Starting At $299</p>
 
-      <div className="content">
-        <h1>Luxurious Rooms</h1>
-        <p>Deluxe Rooms Starting At $299</p>
-
-        <div className="login-buttons">
-          <button className="owner-login"><a href="/owner-login">Owner Login</a></button>
-          <button className="user-login"><a href="/login">user Login</a></button>
+          <div className="login-buttons">
+            <button className="owner-login">
+              <a href="/owner-login">Owner Login</a>
+            </button>
+            <button className="user-login">
+              <a href="/login">User Login</a>
+            </button>
+          </div>
+        </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
--- client/src/component/LandingSite/Navbar.jsx
+++ client/src/component/LandingSite/Navbar.jsx
@@ -1,28 +1,49 @@
-import "../../component/Component.css"
-    import { Link } from "react-router-dom";
+import "../../component/LandingSite/Navbar.css";
+import { Link } from "react-router-dom";
 
-    function Navbar() {
-        return (
-          <nav className="navbar fixed-top navbar-light navbg">
-            <Link to="/" className="navbar-brand mx-3">
-              <b>Room Booking</b>
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg fixed-top navbar-light navbg">
+      <Link to="/" className="navbar-brand mx-3">
+        <b>Room Booking</b>
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Home
             </Link>
-            <div className="ml-auto d-flex">
-              <Link to="/" className="nav-link mx-2">
-                Home
-              </Link>
-              <Link to="/" className="nav-link mx-2">
-                Services
-              </Link>
-              <Link to="/owner-login" className="nav-link mx-2">
-                Owner-login
-              </Link>
-              <Link to="/login" className="nav-link mx-2">
-                Login
-              </Link>
-            </div>
-          </nav>
-        );
-      }
-      
-      export { Navbar };
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Services
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/owner-login" className="nav-link">
+              Owner-login
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/login" className="nav-link">
+              Login
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export { Navbar };
